39844ce3d88e46e6614bb2867990df64d9c63522
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-09-26 00:54:22 +0900

    add public ip address on ec2

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,6 +3,8 @@ resource "aws_instance" "deepracer_instance" {
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
 
+  associate_public_ip_address = true
+
   security_groups = [var.security_group]
 
   iam_instance_profile = var.iam_instance_profile
