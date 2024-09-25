866835c23ce403e4d1fba50bd825c2ac4f12410b
sakuyacatcat <shingo.takagi0116@gmail.com>
2024-09-26 01:06:44 +0900

    update for ssm agent install

--- modules/ec2/user_data.sh
+++ modules/ec2/user_data.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-# ローカル環境に必要なセットアップ
+
+sudo snap install amazon-ssm-agent --classic
+sudo systemctl enable snap.amazon-ssm-agent.amazon-ssm-agent.service
+sudo systemctl start snap.amazon-ssm-agent.amazon-ssm-agent.service
+
 git clone https://github.com/aws-deepracer-community/deepracer-for-cloud.git /home/ubuntu/deepracer-for-cloud
 cd /home/ubuntu/deepracer-for-cloud && ./bin/prepare.sh
